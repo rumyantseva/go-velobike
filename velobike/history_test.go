bc2841a3a8b1a46c71e2badf765b28e022c9f5b2
Elena Grahovac <elena@grahovac.me>
2020-09-02 22:39:31 +0200

    One more test case for history

--- velobike/history_test.go
+++ velobike/history_test.go
@@ -25,4 +25,19 @@ func TestHistoryService_Get(t *testing.T) {
 		_, _, err = client.History.Get()
 		require.NoError(t, err)
 	}
+
+	// Test against a mocked API with fixtures, empty history
+	{
+		f, err := os.Open("../fixtures/history_empty.json")
+		require.NoError(t, err)
+		ts := httptest.NewServer(
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					io.Copy(w, f)
+				}))
+		defer ts.Close()
+		client := NewClient(WithBaseURL(ts.URL))
+		_, _, err = client.History.Get()
+		require.NoError(t, err)
+	}
 }
